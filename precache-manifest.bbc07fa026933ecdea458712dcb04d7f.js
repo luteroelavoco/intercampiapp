fddea863465ffb1adfb941dfece212a8f5ae45fb
cientista1 <40244138+cientista1@users.noreply.github.com>
2020-02-22 21:40:45 -0300

    Updates

---
+++ precache-manifest.bbc07fa026933ecdea458712dcb04d7f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6ac85e204cd737ba31cbfc132ff300",
+    "url": "/intercampiapp/index.html"
+  },
+  {
+    "revision": "db6f1e473c262538caf4",
+    "url": "/intercampiapp/static/css/2.06565635.chunk.css"
+  },
+  {
+    "revision": "e5f87242a64a9a72d218",
+    "url": "/intercampiapp/static/css/main.820bbd79.chunk.css"
+  },
+  {
+    "revision": "db6f1e473c262538caf4",
+    "url": "/intercampiapp/static/js/2.4e3ea3f6.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/intercampiapp/static/js/2.4e3ea3f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5f87242a64a9a72d218",
+    "url": "/intercampiapp/static/js/main.669217e0.chunk.js"
+  },
+  {
+    "revision": "ef884f548e88c1873db9",
+    "url": "/intercampiapp/static/js/runtime-main.e89bcd12.js"
+  },
+  {
+    "revision": "3ab4067f95b42c0f73c04033b6f6d483",
+    "url": "/intercampiapp/static/media/capa.3ab4067f.png"
+  },
+  {
+    "revision": "f4f2df07f396e98f56dd6fdf1a9d28eb",
+    "url": "/intercampiapp/static/media/logo.f4f2df07.png"
+  }
+]);
