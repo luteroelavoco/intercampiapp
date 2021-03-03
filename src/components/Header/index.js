38dfa3f4c4a43fb300f5da84142d824166954491
cientista1 <40244138+cientista1@users.noreply.github.com>
2021-03-03 19:50:56 -0300

    feat: add email on header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -72,7 +72,7 @@ export default function Headers() {
                 <div className="right" data-aos="fade-left"
                      data-aos-offset="300"
                      data-aos-easing="ease-in-sine">
-                    <label style={{ color: 'white' }}>  Contatar em projetocasaviva@gmail.com</label>
+                    <label style={{ color: 'white' }}>  Contatar em projetocasaviva.unilab@gmail.com</label>
                 </div>
             </div>
             <div className="header" id="myHeader">
